c73f77a708bad6938e2556ca767434b8fbbfd6f6
yusseno <yusseno19@gmail.com>
2024-03-05 18:22:14 +0700

    inital test web

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+
+const Layout = () => {
+    return (
+        <div className="scroll-smooth overflow-hidden">
+            <Header />
+        </div>
+    )
+};
+
+export default Layout;
