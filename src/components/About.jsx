c6bf683c365f8ad666ca31e2fe946459f8cdbcec
yusseno <yusseno19@gmail.com>
2024-03-06 15:16:50 +0700

    change font size about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
                     </div>
                 </figure>
                 <article className="mt-[310px] lg:mt-0 lg:mr-4">
-                    <p className="text-slate-400 text-justify text-[16px]"> &nbsp; Hallo! Nama saya Yusseno dan saya senang membuat hal-hal yang hidup di internet. Ketertarikan saya pada pengembangan web dimulai Sekolah Menengah ketika saya mendapatkan tawaran untuk mencoba mengedit template untuk mengerjakan tugas teman saya, berupa tentang HTML & CSS!. <br />
+                    <p className="text-slate-400 text-xs lg:text-sm text-justify text-[16px]"> &nbsp; Hallo! Nama saya Yusseno dan saya senang membuat hal-hal yang hidup di internet. Ketertarikan saya pada pengembangan web dimulai Sekolah Menengah ketika saya mendapatkan tawaran untuk mencoba mengedit template untuk mengerjakan tugas teman saya, berupa tentang HTML & CSS!. <br />
                         &nbsp; Kemudian kareba merasakan tertarik di bidang tersebut saya memutuskan masuk ke sekolah kejuruan dan memutuskan melanjutkan studi ke Universitas untuk menpelajari lebih lanjut minat saya.<br />
                         &nbsp; Berikut adalah beberapa teknologi yang telah saya gunakan baru-baru ini diantaranya :
                     </p>
@@ -48,87 +48,84 @@ const About = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#A9A9A9" class="w-6 h-6 mt-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                         </svg>
-                        <p className="text-slate-400 ml-2 mt-2">Backend</p>
+                        <p className="text-slate-400 text-sm ml-2 mt-3">Backend</p>
                     </div>
                     <div className="grid grid-cols-2 lg:grid-cols-5 gap-1">
                         <div className="flex flex-col items-center justify-center">
                             <img src={Golang} alt='kosong' className="h-6 mt-2"></img>
-                            <p className="text-slate-400 mt-2">Golang</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">Golang</p>
                         </div>
                         <div className="flex flex-col items-center justify-center">
                             <img src={NextJs} alt='kosong' className="h-6 mt-2"></img>
-                            <p className="text-slate-400 mt-2">Next Js</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">Next Js</p>
                         </div>
                         <div className="flex flex-col items-center justify-center">
                             <img src={NestJs} alt='kosong' className="h-6 mt-2"></img>
-                            <p className="text-slate-400 mt-2">Nest Js</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">Nest Js</p>
                         </div>
                         <div className="flex flex-col items-center justify-center">
                             <img src={Laravel} alt='kosong' className="h-6 mt-2"></img>
-                            <p className="text-slate-400 mt-2">Laravel</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">Laravel</p>
                         </div>
                     </div>
-                    <br />
                     <div className="flex">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#A9A9A9" class="w-6 h-6 mt-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                         </svg>
-                        <p className="text-slate-400 ml-2 mt-2">Frontend</p>
+                        <p className="text-slate-400 text-sm ml-2 mt-3">Frontend</p>
                     </div>
                     <div className="grid grid-cols-2 lg:grid-cols-5 gap-1">
                         <div className="flex flex-col items-center justify-center">
                             <img src={Reacts} alt='kosong' className="h-6 mt-2"></img>
-                            <p className="text-slate-400 mt-2">React</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">React</p>
                         </div>
                         <div className="flex flex-col items-center justify-center">
                             <img src={Tailwind} alt='kosong' className="h-5 mt-2"></img>
-                            <p className="text-slate-400 text-[14px] lg:text-sm mt-2 lg:mt-4">Tailwind CSS</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">Tailwind CSS</p>
                          </div>
                     </div>
-                    <br />
                     <div className="flex">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#A9A9A9" class="w-6 h-6 mt-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                         </svg>
-                        <p className="text-slate-400 ml-2 mt-2">Development dan Operations</p>
+                        <p className="text-slate-400 text-sm ml-2 mt-3">Development & Operations</p>
                     </div>
                     <div className="grid grid-cols-2 lg:grid-cols-5 gap-1">
                         <div className="flex flex-col items-center justify-center">
                             <img src={Ubuntu} alt='kosong' className="h-6 mt-2"></img>
-                            <p className="text-slate-400 mt-2">Ubuntu</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">Ubuntu</p>
                         </div>
                         <div className="flex flex-col items-center justify-center">
                             <img src={Gitlab} alt='kosong' className="h-5 mt-2"></img>
-                            <p className="text-slate-400 mt-2">Gitlab</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">Gitlab</p>
                         </div>
                         <div className="flex flex-col items-center justify-center">
                             <img src={Docker} alt='kosong' className="h-5 mt-2"></img>
-                            <p className="text-slate-400 mt-2">Docker</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">Docker</p>
                         </div>
                     </div>
-                    <br />
                     <div className="flex">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#A9A9A9" class="w-6 h-6 mt-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                         </svg>
-                        <p className="text-slate-400 ml-2 mt-2">Database</p>
+                        <p className="text-slate-400 text-sm ml-2 mt-3">Database</p>
                     </div>
                     <div className="grid grid-cols-2 lg:grid-cols-5 gap-1">
                         <div className="flex flex-col items-center justify-center">
                             <img src={MySql} alt='kosong' className="h-6 mt-2"></img>
-                            <p className="text-slate-400 mt-2">MySql</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">MySql</p>
                         </div>
                         <div className="flex flex-col items-center justify-center">
                             <img src={PostresSql} alt='kosong' className="h-5 mt-2"></img>
-                            <p className="text-slate-400 mt-2">PostgresSQL</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">PostgresSQL</p>
                         </div>
                         <div className="flex flex-col items-center justify-center">
                             <img src={MongoDb} alt='kosong' className="h-5 mt-2"></img>
-                            <p className="text-slate-400 mt-2">MongoDB</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">MongoDB</p>
                         </div>
                         <div className="flex flex-col items-center justify-center">
                             <img src={Redist} alt='kosong' className="h-5 mt-2"></img>
-                            <p className="text-slate-400 mt-2">Redist</p>
+                            <p className="text-slate-400 text-xs lg:text-sm mt-2">Redist</p>
                         </div>
                     </div>
                 </article>
